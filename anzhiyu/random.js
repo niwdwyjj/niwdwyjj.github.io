28163db2c590ba27e1935fdd537188554f861ee5
niwdwyjj <deep_57@163.com>
2024-08-21 08:02:03 +0800

    Site updated: 2024-08-21 08:02:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["artciles/2024/ba1f84a6/","artciles/2024/4a17b156/","artciles/2024/585b44c/"];function toRandomPost(){
+var posts=["artciles/ba1f84a6/","artciles/585b44c/","artciles/4a17b156/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
