0e7f27843b981934e024f90dae808522b70b04bc
niwdwyjj <deep_57@163.com>
2024-08-26 02:02:43 +0800

    Site updated: 2024-08-26 02:02:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["artciles/2024/585b44c/","artciles/2024/ba1f84a6/","artciles/2024/7d306bdf/","artciles/2024/4a17b156/"];function toRandomPost(){
+var posts=["artciles/2024/585b44c/","artciles/2024/7d306bdf/","artciles/2024/ba1f84a6/","artciles/2024/4a17b156/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
